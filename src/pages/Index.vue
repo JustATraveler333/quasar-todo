10d522a1c4eaa84ccf9c05a53feb1874572a59e4
Matthew Cullum <78925506+JustATraveler333@users.noreply.github.com>
2021-12-19 15:08:03 -0600

    first commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-select
+      v-model="selected"
+      :options="['luke', 'matt', 'jake']"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
